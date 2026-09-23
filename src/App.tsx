8b600f419f512f6b7eee474f52a8812e5f00bf2d
alisher134 <alisherrakhmanov134@gmail.com>
2026-09-23 15:33:01 +0500

    feat: integrate shadcn UI and add base button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-2xl font-semibold">Hello world</h1>
+      <Button>Shadcn Button</Button>
+    </div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
@@ -6,4 +7,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
 });
